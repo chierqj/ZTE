c7b17292afdca79d14561601baf2141807de478c
chier <chier@chierdeMacBook-Pro.local>
2020-04-19 19:24:06 +0800

    暴力占道 50.863

--- src/main.cpp
+++ src/main.cpp
@@ -15,32 +15,53 @@ struct Good;
 
 // 站点
 struct Station {
+  Station(const std::string &_strID, const int &_ID, const int &_pickers)
+      : strID(_strID), ID(_ID), pickers(_pickers), leftPickers(_pickers) {}
   void Print() {
     std::cerr << "@ Station: (";
     std::cerr << strID << ", " << ID << ", " << pickers << ")\n";
   }
-  std::string strID;                                     // 站点ID
-  int ID;                                                // 映射ID
-  int pickers;                                           // 拣货员数量
-  int leftPickers;                                       // 剩余捡货员
-  std::unordered_map<int, std::bitset<80>> partPickers;  // 轨道id捡货员状态
+  std::string strID;  // 站点ID
+  int ID;             // 映射ID
+  int pickers;        // 拣货员数量
+  int leftPickers;    // 剩余捡货员
 };
 // 轨道
 struct Road {
+  Road(const std::string &_strID, const int &_ID, Station *_station0,
+       Station *_station1)
+      : strID(_strID),
+        ID(_ID),
+        station0(_station0),
+        station1(_station1),
+        loadedWeight(std::vector<double>(80, 0)) {}
   void Print() {
     std::cerr << "@ Road: (";
     std::cerr << strID << ", " << ID << ") {";
     std::cerr << station0->strID << ", " << station1->strID << "}\n";
+  }
+  std::bitset<80> &GetPickerStatus(const Station *sta) {
+    if (sta->ID == station0->ID) return pickerStatusFrom;
+    return pickerStatusTo;
+  }
+  std::bitset<80> &GetPickerStatus(const int &ID) {
+    if (ID == station0->ID) return pickerStatusFrom;
+    return pickerStatusTo;
   }
 
   std::string strID;                 // 轨道ID
   int ID;                            // 映射ID
   Station *station0;                 // 连接站点0
   Station *station1;                 // 连接站点1
+  std::bitset<80> pickerStatusFrom;  // station0的捡货员状态
+  std::bitset<80> pickerStatusTo;    // station1的捡货员状态
   std::vector<double> loadedWeight;  // 每个车道已装载重量
 };
 // 货物
 struct Good {
+  Good(const std::string &_strID, const int &_ID, Station *_start,
+       Station *_end, const double &_quality)
+      : strID(_strID), ID(_ID), start(_start), end(_end), quality(_quality) {}
   void Print() {
     std::cerr << "@ Good: (";
     std::cerr << strID << ", " << ID << ", " << quality << ") (";
@@ -86,8 +107,12 @@ class XJBG {
   void OuputAnswer();                      // 输出答案
 
  private:
-  std::unordered_map<std::string, int> HashStation;  // 站点映射
-  std::unordered_map<std::string, int> HashRoad;     // 道路映射
+  void splitString(const std::string &s, std::vector<std::string> &v,
+                   const std::string &c);  // 分割字符串
+
+ private:
+  std::unordered_map<std::string, Station *> HashStation;  // 站点映射
+  std::unordered_map<std::string, Road *> HashRoad;        // 道路映射
 
  private:
   int m_StationNum;                   // 站点数目
@@ -117,80 +142,56 @@ void XJBG::Print() {
 #endif
 }
 
+void XJBG::splitString(const std::string &s, std::vector<std::string> &v,
+                       const std::string &c) {
+  v.clear();
+  std::string::size_type pos1, pos2;
+  pos2 = s.find(c);
+  pos1 = 0;
+  while (std::string::npos != pos2) {
+    v.push_back(s.substr(pos1, pos2 - pos1));
+    pos1 = pos2 + c.size();
+    pos2 = s.find(c, pos1);
+  }
+  if (pos1 != s.length()) v.push_back(s.substr(pos1));
+}
+
 void XJBG::LoadData() {
   std::string line;
   std::cin >> line;
   char c;
   std::stringstream ss(line);
   ss >> m_StationNum >> c >> m_RoadNum >> c >> m_LaneNum >> c >> m_Capacity;
+  std::vector<std::string> info;
   for (int i = 0; i < m_StationNum; ++i) {
     std::cin >> line;
-    int pos = line.find(',');
-    std::string x = line.substr(0, pos);
-    int y = std::stod(line.substr(pos + 1, line.size() - pos - 1));
-    Station *station = new Station;
-    station->strID = x;
-    station->ID = i;
-    station->pickers = y;
-    station->leftPickers = y;
+    this->splitString(line, info, ",");
+    int y = std::stoi(info[1]);
+    Station *station = new Station(info[0], i, y);
     m_stations.emplace_back(station);
-    HashStation[station->strID] = station->ID;
+    HashStation[station->strID] = station;
     m_Pickers += y;
   }
   for (int i = 0; i < m_RoadNum; ++i) {
     std::cin >> line;
-    int pos = line.find(',');
-    std::string x = line.substr(0, pos);
-    line = line.substr(pos + 1, line.size() - pos - 1);
-    pos = line.find(',');
-    std::string y = line.substr(0, pos);
-    std::string z = line.substr(pos + 1, line.size() - pos - 1);
-    Road *road = new Road;
-    road->strID = x;
-    road->ID = i;
-    road->station0 = m_stations[HashStation[y]];
-    road->station1 = m_stations[HashStation[z]];
-    road->loadedWeight = std::vector<double>(80, 0);
+    this->splitString(line, info, ",");
+    Road *road =
+        new Road(info[0], i, HashStation[info[1]], HashStation[info[2]]);
     m_roads.emplace_back(road);
-    HashRoad[road->strID] = road->ID;
+    HashRoad[road->strID] = road;
   }
   std::cin >> line;
   m_GoodNum = std::stoi(line);
   for (int i = 0; i < m_GoodNum; ++i) {
     std::cin >> line;
-    int pos = line.find(',');
-    std::string id = line.substr(0, pos);
-    line = line.substr(pos + 1, line.size() - pos - 1);
-    pos = line.find(',');
-    std::string st = line.substr(0, pos);
-    line = line.substr(pos + 1, line.size() - pos - 1);
-    pos = line.find(',');
-    std::string ed = line.substr(0, pos);
-    line = line.substr(pos + 1, line.size() - pos - 1);
-    pos = line.find(',');
-    double x = std::stod(line.substr(0, pos));
-    line = line.substr(pos + 1, line.size() - pos - 1);
-    std::vector<Station *> vt;
-    while (true) {
-      pos = line.find(',');
-      if (pos == std::string::npos) {
-        if (line != "null") {
-          vt.emplace_back(m_stations[HashStation[line]]);
-        }
-        break;
+    this->splitString(line, info, ",");
+    Good *good = new Good(info[0], i, HashStation[info[1]],
+                          HashStation[info[2]], std::stod(info[3]));
+    if (info[4] != "null") {
+      for (int j = 4; j < info.size(); ++j) {
+        good->requiredStations.emplace_back(HashStation[info[j]]);
       }
-      std::string s = line.substr(0, pos);
-      vt.emplace_back(m_stations[HashStation[s]]);
-      line = line.substr(pos + 1, line.size() - pos - 1);
     }
-    Good *good = new Good;
-    good->strID = id;
-    good->ID = i;
-    Station *station = m_stations[HashStation[st]];
-    good->start = station;
-    good->end = m_stations[HashStation[ed]];
-    good->quality = x;
-    good->requiredStations = vt;
     m_goods.emplace_back(good);
   }
   this->Print();
@@ -208,7 +209,7 @@ void XJBG::BuildGraph() {
   }
   std::sort(m_goods.begin(), m_goods.end(),
             [&](const Good *g1, const Good *g2) {
-              return g1->quality > g2->quality;
+              return g1->quality < g2->quality;
             });
 }
 
@@ -242,28 +243,43 @@ void XJBG::OuputAnswer() {
 }
 
 bool XJBG::Dijkstra(Good *good, int laneNum) {
-  std::queue<int> Q;
-  Q.push(good->start->ID);
+  std::queue<Station *> Q;
+  Q.push(good->start);
   std::vector<bool> vis(m_StationNum, false);
   std::vector<int> prevc(m_StationNum, 0);
   vis[good->start->ID] = true;
   bool ok = false;
   while (!Q.empty()) {
-    int head = Q.front();
+    Station *head = Q.front();
     Q.pop();
-    if (head == good->end->ID) {
+    if (head->ID == good->end->ID) {
       ok = true;
       break;
     }
-    for (auto &it : Graph[head]) {
-      const Station *nxtsta = it.first;
-      const Road *nxtroad = it.second;
-      if (vis[nxtsta->ID] || nxtsta->pickers < 8) continue;
-      if (nxtroad->loadedWeight[laneNum] + good->quality <= m_Capacity - 1) {
+    for (auto &it : Graph[head->ID]) {
+      Station *nxtsta = it.first;
+      Road *nxtroad = it.second;
+      // 已经拓展过
+      if (vis[nxtsta->ID]) continue;
+      // 重量超载
+      if (nxtroad->loadedWeight[laneNum] + good->quality > m_Capacity) continue;
+      auto &pickFrom = nxtroad->GetPickerStatus(head);
+      auto &pickTo = nxtroad->GetPickerStatus(nxtsta);
+      // 不需要捡货员
+      if (pickTo[laneNum] && pickFrom[laneNum]) {
         vis[nxtsta->ID] = true;
-        prevc[nxtsta->ID] = head;
-        Q.push(nxtsta->ID);
+        prevc[nxtsta->ID] = head->ID;
+        Q.push(nxtsta);
+        continue;
       }
+      if (head->leftPickers <= 0 && !pickFrom[laneNum]) continue;
+      if (nxtsta->leftPickers <= 0 && !pickTo[laneNum]) continue;
+      if (!pickFrom[laneNum]) --head->leftPickers;
+      if (!pickTo[laneNum]) --nxtsta->leftPickers;
+      pickFrom[laneNum] = pickTo[laneNum] = 1;
+      vis[nxtsta->ID] = true;
+      prevc[nxtsta->ID] = head->ID;
+      Q.push(nxtsta);
     }
   }
   if (!ok) return false;
@@ -273,7 +289,9 @@ bool XJBG::Dijkstra(Good *good, int laneNum) {
     Road *road = RoadTable[now][cur];
     road->loadedWeight[laneNum] += good->quality;
     if (road->loadedWeight[0] > m_Capacity) {
-      std::cerr << road->loadedWeight[0] << "\n";
+      std::cerr << "Error [Train Weight OverLoad!]\n";
+      good->Print();
+      road->Print();
     }
     good->roadPath.emplace_back(road);
     good->lanePath.emplace_back(laneNum);
@@ -284,15 +302,12 @@ bool XJBG::Dijkstra(Good *good, int laneNum) {
 
 void XJBG::ArrangePath() {
   int count = 0;
-  std::vector<bool> vis(m_GoodNum, false);
-  for (int i = 0; i < m_LaneNum; ++i) {
-    for (auto &good : m_goods) {
-      if (vis[good->ID]) continue;
-      if (good->start->pickers < 0 || good->end->pickers < 0) continue;
+  for (auto &good : m_goods) {
+    for (int i = 0; i < m_LaneNum; ++i) {
       if (good->requiredStations.empty()) {
         if (this->Dijkstra(good, i)) {
           ++count;
-          vis[good->ID] = true;
+          break;
         }
       }
     }
